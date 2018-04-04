9be38731d07f843fa3a912977360506e4e28c728
Nathan Oyama <nate@culip.net>
2018-04-04 12:15:54 -0600

    Added HelloWrold Java stuff -Nathan

---
+++ helloWorld.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author nate
+ */
+public class helloWorld {
+    
+    public static void main(String[] args){
+        
+        for (int i=1; i <= 10; i++) {
+            
+            System.err.println("Hello Nathans"+i);
+            
+        }
+        
+    }
+    
+}
